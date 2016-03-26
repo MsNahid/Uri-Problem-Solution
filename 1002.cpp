809dcd0fc02d6a67697efc271b7fa951ec910fce
MsNahid <mssnahid729@gmail.com>
2016-03-26 20:48:05 +0600

    Initial

---
+++ 1002.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <cstdio>
+#include <cmath>
+
+using namespace std;
+
+int main(){
+    double r,a;
+    scanf("%lf", &r);
+    a = 3.14159 * ( r * r);
+    printf("A=%0.4lf\n", a);
+    return 0;
+}
