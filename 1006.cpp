137559f949c82e068833127bc6f5eaa7ad5bc940
MsNahid <mssnahid729@gmail.com>
2016-03-29 22:49:40 +0600

    Added files via upload

---
+++ 1006.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <cstdio>
+
+using namespace std;
+
+int main(){
+
+    double a, b,c;
+    scanf("%lf %lf %lf", &a, &b, &c);
+    printf("MEDIA = %0.1lf\n", (a * 2 + b * 3 + c * 5) / (2 + 3 + 5));
+    return 0;
+}
