d995b68c2f0dc2c4c10b8c132c43d53a8d0fc85e
MsNahid <mssnahid729@gmail.com>
2016-03-29 23:01:03 +0600

    Added files via upload

---
+++ 1016.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <cstdio>
+
+using namespace std;
+
+int main(){
+
+    int distance;
+    scanf("%d", &distance);
+    printf("%d minutos\n", 2 * distance);
+    return 0;
+}
